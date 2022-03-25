c8636c3c6275d69d91d8220ed1e6190f3b246a9f
DrXoo <domgaromero@gmail.com>
2022-03-26 00:36:29 +0100

    Deployed!

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "d6d316ee93ff41b09beaf3cfb919c078";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3
     }
   }
 };
